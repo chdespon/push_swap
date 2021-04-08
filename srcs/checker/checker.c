3e083687d83d80d1e81e0800d678f5502659e99b
chdespon <chdespon@student.42.fr>
2021-04-08 17:01:38 +0200

    push-swap

--- srcs/checker/checker.c
+++ srcs/checker/checker.c
@@ -6,7 +6,7 @@
 /*   By: chdespon <chdespon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 14:34:29 by chdespon          #+#    #+#             */
-/*   Updated: 2021/04/02 13:03:09 by chdespon         ###   ########.fr       */
+/*   Updated: 2021/04/08 14:45:52 by chdespon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ int	main(int ac, char **av)
 {
 	t_engine	*engine;
 
-	if (ac == 1)
-		return (0);
 	engine = malloc_engine();
-	if (ft_strcmp(av[1], "-v") == 0)
+	if (ac > 1 && ft_strcmp(av[1], "-v") == 0)
 		engine->verbose = true;
+	if (ac == 1 + (int)engine->verbose)
+		return (0);
 	parse_arg(ac, av + 1, engine);
 	read_output(engine);
 	set_stack_a(engine, av + 1 + engine->verbose);
--- srcs/push_swap/resolve_stack.c
+++ srcs/push_swap/resolve_stack.c
@@ -6,20 +6,39 @@
 /*   By: chdespon <chdespon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 13:15:05 by chdespon          #+#    #+#             */
-/*   Updated: 2021/04/06 16:46:48 by chdespon         ###   ########.fr       */
+/*   Updated: 2021/04/08 15:27:23 by chdespon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int		op_num(t_list *stack_b, t_engine *engine)
+{
+	t_list *tmp;
+
+	tmp = stack_b;
+	while (stack_b)
+	{
+		
+	}
+}
+
 void	find_best_num(t_engine *engine)
 {
 	t_list *tmp_a;
+	t_list op;
+	int		i;
 
 	tmp_a = engine->stack_b;
-	while (engine->stack_b != NULL)
+	i = 0;
+	while (i < engine->len_stack_b)
 	{
-		
+		if (op_num(engine->stack_b, engine) >
+			op_num(engine->stack_b->next, engine))
+		{
+			rotate(&engine->stack_b, engine->len_stack_b);
+			ft_lst_add_back(&op, ft_lst_create_node("rb"));
+		}
 	}
 }
 
@@ -38,7 +57,7 @@ void	resolve_stack(t_engine *engine)
 	index = engine->len_stack_b - 1;
 	while (engine->stack_b != NULL)
 	{
-		find_best_num(engine);
+		// find_best_num(engine);
 		if (index != ((t_data_num*)engine->stack_b->data)->index)
 		{
 			rotate(&engine->stack_b, engine->len_stack_b);
